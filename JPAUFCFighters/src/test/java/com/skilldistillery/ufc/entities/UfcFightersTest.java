5d03f3a33b316ddd24da005598d81e24af0ccfe8
Johnelle Walker <johnellewalker@gmail.com>
2024-01-29 04:19:29 -0700

    final commit

--- JPAUFCFighters/src/test/java/com/skilldistillery/ufc/entities/UfcFightersTest.java
+++ JPAUFCFighters/src/test/java/com/skilldistillery/ufc/entities/UfcFightersTest.java
@@ -2,6 +2,7 @@ package com.skilldistillery.ufc.entities;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
@@ -43,6 +44,18 @@ class UfcFightersTest {
 	void test() {
 		assertNotNull(ufcFighters);
 		assertEquals("Islam", ufcFighters.getFirstName());
+		assertEquals("Makhachev", ufcFighters.getLastName());
+		assertEquals(32, ufcFighters.getAge());
+		assertEquals("Russian", ufcFighters.getNationality());
+		assertEquals("Lightweight", ufcFighters.getWeightclass());
+		assertEquals(25, ufcFighters.getWins());
+		assertEquals(1, ufcFighters.getLosses());
+		assertEquals(0, ufcFighters.getDraws());
+		assertEquals(5, ufcFighters.getKnockouts());
+		assertEquals(11, ufcFighters.getSubmissions());
+		assertEquals(9, ufcFighters.getDecisions());
+		assertTrue(ufcFighters.getChampion(), "Test case should be true");
+		assertTrue(ufcFighters.getActive(), "Test case should be active");
 
 	}
 
